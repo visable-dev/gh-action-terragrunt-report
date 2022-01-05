04b1d66bbb4f253e8891531f120c06fabca2151b
Andreas Litt <andreas.litt@visable.com>
2022-01-05 09:02:53 +0100

    feat: create checks instead of comments (#3)
    in order to have a better overview if many state files are present

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@ import * as core from '@actions/core';
 import * as github from '@actions/github';
 import * as glob from '@actions/glob';
 import {PullRequestEvent} from '@octokit/webhooks-definitions/schema';
+import {Endpoints} from '@octokit/types';
 import {promises} from 'fs';
 
 const inputs = {
   github_token: core.getInput('github_token', {required: true}),
   diff_file_suffix: core.getInput('diff_file_suffix', {required: true}),
   search_path: core.getInput('search_path', {required: true}),
+  pretty_name_regex: core.getInput('pretty_name_regex', {required: false}),
+  pretty_name_separator: core.getInput('pretty_name_separator', {
+    required: false,
+  }),
 };
 
 const ctx = github.context;
@@ -21,7 +26,13 @@ const errorHandler: NodeJS.UncaughtExceptionListener = error => {
 process.on('uncaughtException', errorHandler);
 process.on('unhandledRejection', errorHandler);
 
-const diffSummaryRegex =
+let prettyNameRegex: RegExp | null = null;
+if (inputs.pretty_name_regex !== '') {
+  prettyNameRegex = RegExp(inputs.pretty_name_regex);
+  core.info(`Using regex ${prettyNameRegex} to prettify name.`);
+}
+
+const diffChangeRegex =
   /^Plan: (\d+) to add, (\d+) to change, (\d+) to destroy./m;
 
 async function run() {
@@ -34,7 +45,9 @@ async function run() {
     throw 'Cannot execute action on closed pull request!';
   }
 
-  const prLines = [];
+  const checks: Array<
+    Endpoints['POST /repos/{owner}/{repo}/check-runs']['parameters']
+  > = [];
 
   const globber = await glob.create(
     `${inputs.search_path}/**/*${inputs.diff_file_suffix}`
@@ -43,48 +56,90 @@ async function run() {
     core.info(`Processing file ${file}`);
 
     const diff = `${await promises.readFile(file)}`;
-    core.info(diff);
-    const summary = diff.match(diffSummaryRegex);
+    let plan = {
+      add: 0,
+      change: 0,
+      destroy: 0,
+    };
 
-    if (summary === null) {
-      throw `File ${file} has wrong format! Please ensure that \`diff_file_suffix\` only points to valid diff files.`;
+    if (
+      !diff.match('No changes. Your infrastructure matches the configuration.')
+    ) {
+      // Diff is supposed to contain some changes
+      const res = diff.match(diffChangeRegex);
+      if (res === null) {
+        console.error(diff);
+        throw `File ${file} has wrong format! Please ensure that \`diff_file_suffix\` only points to valid diff files.`;
+      }
+      plan = {
+        add: Number(res[1]),
+        change: Number(res[2]),
+        destroy: Number(res[3]),
+      };
     }
-    const summaryLine = `##### Plan: \`${summary[1]}\` to add, \`${summary[2]}\` to change, \`${summary[3]}\` to destroy.`;
-    core.info(`SummaryLine: ${summaryLine}`);
+
+    const summary = `Plan: ${plan.add} to add, ${plan.change} to change, ${plan.destroy} to destroy.`;
+
+    core.info(`Summary: ${summary}`);
 
     const prettyFilename = file.replace(inputs.search_path, '');
-    prLines.push(`
-#### \`${prettyFilename}\`:
-${summaryLine}
-<details><summary>Full Plan</summary>
+    let name = prettyFilename;
 
+    if (prettyNameRegex) {
+      const matches = prettyFilename.match(prettyNameRegex);
+      if (matches) {
+        name = matches
+          .slice(1) // Skip first entry
+          .filter(n => n) // filter undefined capture groups
+          .join(inputs.pretty_name_separator); // join with defined separator
+      } else {
+        core.warning(
+          `No match found for filename '${prettyFilename} with pretty_name_regex ${prettyNameRegex}.'`
+        );
+      }
+    }
+
+    let conclusion: 'success' | 'neutral' = 'success';
+    if (plan.add > 0 || plan.change > 0 || plan.destroy > 0) {
+      conclusion = 'neutral';
+    }
+    checks.push({
+      ...ctx.repo,
+      head_sha: pullRequest.head.sha,
+      name: name,
+      conclusion: conclusion,
+      output: {
+        title: summary,
+        summary: `Please find below the full plan for \`${prettyFilename}\`.`,
+        text: `
 \`\`\`terraform
 ${diff}
 \`\`\`
-</details>
-`);
+`,
+      },
+    });
   }
-  if (prLines.length === 0) {
-    prLines.push(`
-#### :warning: No matching diff file found!
-
-Please read the [setup instructions](https://github.com/littldr/gh-action-terragrunt-report#usage) and ensure that you configured terragrunt correctly!
-`);
+  if (checks.length === 0) {
+    checks.push({
+      ...ctx.repo,
+      head_sha: ctx.sha,
+      name: 'Terragrunt Report',
+      conclusion: 'failure',
+      output: {
+        title: 'No diff files found!',
+        summary: 'No diff files found!',
+        text: 'Please read the [setup instructions](https://github.com/visable-dev/gh-action-terragrunt-report#usage) and ensure that you configured terragrunt correctly!',
+      },
+    });
   }
-  const body = `
-### Run #[${ctx.runId}](https://github.com/${ctx.repo.owner}/${
-    ctx.repo.repo
-  }/actions/runs/${ctx.runId}) from ${ctx.sha} on \`${ctx.ref}\`
-
-${prLines.join('\n')}
-`;
 
   const octokit = github.getOctokit(inputs.github_token);
-  await octokit.rest.issues.createComment({
-    ...ctx.repo,
-    issue_number: pullRequest.number,
-    body,
-  });
+  for (const check of checks) {
+    const result = await octokit.rest.checks.create(check);
+    core.info(
+      `Created check ${result.data.name} (${result.data.id}): ${result.data.html_url}`
+    );
+  }
 }
 
 run();
